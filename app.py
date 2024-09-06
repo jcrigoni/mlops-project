10ab565bc9c22366346c5f6c9ec2e64c22517d6b
jcrigoni <jcrigoni@gmail.com>
2024-09-06 11:59:39 +0200

    corrected input standardization

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ def model_pred(features):
     prediction = model.predict(test_data)
     return int(prediction[0])
 
+loa_mean = 4159.68
+loa_std = 1421.40
+
+tdo_mean = 8718.92
+tdo_std = 6627.16
+
+income_mean = 70039.90
+income_std = 20072.21
 
 @app.route("/", methods=["GET"])
 def Home():
@@ -22,9 +30,9 @@ def Home():
 def predict():
     if request.method == "POST":
         credit_lines_outstanding = int(request.form["credit_lines_outstanding"])
-        loan_amt_outstanding = int(request.form["loan_amt_outstanding"])
-        total_debt_outstanding = int(request.form["total_debt_outstanding"])
-        income = float(request.form["income"])
+        loan_amt_outstanding = float(((request.form["loan_amt_outstanding"]) - loa_mean) / loa_std)
+        total_debt_outstanding = float(((request.form["total_debt_outstanding"]) - tdo_mean) / tdo_std)
+        income = float(((request.form["income"]) - income_mean) / income_std)
         years_employed = int(request.form["years_employed"])
         fico_score = int(request.form["fico_score"])
         prediction = model.predict(
@@ -39,7 +47,7 @@ def predict():
 
         else:
             return render_template(
-                "index.html", prediction_text="Yes you can grant a loan to this client. :)"
+                "index.html", prediction_text="Yes you can grant a loan to this client."
             )
 
     else:
